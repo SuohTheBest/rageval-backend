56b0283aabfc5fb19314b9eb606b10e742d5a904
xuyanhan <221900076@smail.nju.edu.cn>
2025-04-22 15:20:28 +0800

    prompt_metrics更新

--- prompt/metrics.py
+++ prompt/metrics.py
@@ -1,10 +1,9 @@
-from zhipuai import ZhipuAI
 from langchain_core.prompts import PromptTemplate
-import os
+from zhipuai import ZhipuAI
 
 metric_prompt = '''
 你是一个prompt评估员。
-接下来，我将给你一个prompt。
+接下来，我将给你一个prompt和模型对prompt的回答。
 请你评估以下维度该prompt的表现，给出打分：
 
 {metric}
@@ -14,22 +13,28 @@ prompt：
 ~~~
 {prompt}
 ~~~
+待评估的回答：
+~~~
+{answer}
+~~~
 你应该返回给我一个数字，值是该维度对应的评估打分。
 不要输出任何其他内容。
 '''
 
 
-os.environ["API_KEY"] = "3d75e2b95b1549ada6e0f3ad93c25f72.86QnsnzUWVy0Se7K"
+import os
+
+os.environ["API_KEY"] = "2ac574e73afa430fb225aa3fb48a6fc9.wHZ6jqzAD6ahuEMX"
 
 def get_completion(prompt,model="glm-4-flash",temperature=0):
-    client = ZhipuAI(api_key = os.environ["API_KEY"])
-
+    api_key = os.environ.get('API_KEY')
+    client = ZhipuAI(api_key=api_key)
     response = client.chat.completions.create(
-
         model=model,
         messages=[
             {"role": "user", "content": prompt}
         ],
+        temperature=temperature
     )
     if len(response.choices) > 0:
         return response.choices[0].message.content
@@ -41,6 +46,7 @@ class Metric:
     def __init__(self):
         self.metric = ""
         self.prompt = ""
+        self.answer = ""
 
     def evaluate(self,prompt):
         pass
@@ -52,18 +58,17 @@ class knowledgeSearchCorrectnessMetric(Metric):
         self.prompt = prompt
         self.metric = '''知识查找正确性。评估系统给定的知识片段是否能够对问题做出回答。如果知识片段不能做出回答，打分为0；如果知识片段可以做出回答，打分为1。'''
 
-        # TODO:待评估的回答（调用大模型）
-        # answer = get_completion(self.prompt)
+        self.answer = get_completion(self.prompt)
 
-        final_prompt = PromptTemplate(input_variables=["metric","prompt"],
+        final_prompt = PromptTemplate(input_variables=["metric","prompt","answer"],
                                       template=metric_prompt
                                       )
         response = get_completion(final_prompt.format(
             metric=self.metric,
-            prompt=self.prompt
+            prompt=self.prompt,
+            answer=self.answer
         ))
         return response
-
 
 
 # 回答一致性
@@ -71,16 +76,15 @@ class answerAdherenceMetric(Metric):
     def evaluate(self,prompt):
         self.metric = '''回答一致性。评估系统的回答是否针对用户问题展开，是否有偏题、错误理解题意的情况，打分分值在0~1之间，0为完全偏题，1为完全切题。'''
 
+        self.answer = get_completion(self.prompt)
 
-        # TODO:待评估的回答（调用大模型）
-        # answer = get_completion(self.prompt)
-
-        final_prompt = PromptTemplate(input_variables=["metric","prompt"],
+        final_prompt = PromptTemplate(input_variables=["metric","prompt","answer"],
                                       template=metric_prompt
                                       )
         response = get_completion(final_prompt.format(
             metric=self.metric,
-            prompt=self.prompt
+            prompt=self.prompt,
+            answer=self.answer
         ))
         return response
 
@@ -91,14 +95,20 @@ class DefinitionMetric(Metric):
         self.metric = metric
 
     def evaluate(self,prompt):
-        # answer = get_completion(self.prompt)
+        self.answer = get_completion(self.prompt)
 
-        final_prompt = PromptTemplate(input_variables=["metric","prompt"],
+        final_prompt = PromptTemplate(input_variables=["metric","prompt","answer"],
                                       template=metric_prompt
                                       )
         response = get_completion(final_prompt.format(
             metric=self.metric,
-            prompt=self.prompt
+            prompt=self.prompt,
+            answer=self.answer
         ))
 
         return response
+
+
+
+
+
