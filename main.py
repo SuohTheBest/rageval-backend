f65fca8a5e64c04389daea0c4a715f6da59a6953
骆葆源 <221200004@smail.nju.edu.cn>
2025-04-29 14:25:40 +0800

    update cors and deploy settings

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://http://47.97.175.75:80"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["Content-Type", "Authorization"],
@@ -31,7 +31,3 @@ app.include_router(rag_router.router, prefix=g_prefix)
 @app.get("/")
 async def root():
     return {"message": "Hello World!"}
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=False)
